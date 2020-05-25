2be459f39450b5b320025d6227bee580f19aa074
Johannes B <johannes.bock@live.de>
2020-05-25 13:11:36 +0200

    torpedos

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -23,6 +23,9 @@ namespace Asteroids
     {
         DispatcherTimer timer = new DispatcherTimer();
         List<Asteroid> Asteroiden = new List<Asteroid>();
+        List<Torpedo> Torpedos = new List<Torpedo>();
+        Raumschiff Enterprise;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -41,6 +44,13 @@ namespace Asteroids
                 item.Zeichnen(Zeichenfläche);
                 item.Bewegen(timer.Interval, Zeichenfläche);
             }
+            Enterprise.Zeichnen(Zeichenfläche);
+            Enterprise.Bewegen(timer.Interval, Zeichenfläche);
+            foreach (Torpedo item in Torpedos)
+	        {
+                item.Zeichnen(Zeichenfläche);
+                item.Bewegen(timer.Interval, Zeichenfläche);
+	        }
         }
 
         private void BTN_Start_Click(object sender, RoutedEventArgs e)
@@ -50,7 +60,37 @@ namespace Asteroids
             {
                 Asteroiden.Add(new Asteroid(Zeichenfläche));
             }
+            Enterprise = new Raumschiff(Zeichenfläche);
             BTN_Start.IsEnabled = false;
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(timer.IsEnabled)
+            {
+                switch(e.Key)
+                {
+                    case Key.Left:
+                    case Key.A:
+                        Enterprise.Lenke(true);
+                        break;
+                    case Key.Right:
+                    case Key.D:
+                        Enterprise.Lenke(false);
+                        break;
+                    case Key.Up:
+                    case Key.W:
+                        Enterprise.Beschleunige(true);
+                        break;
+                    case Key.Down:
+                    case Key.S:
+                        Enterprise.Beschleunige(false);
+                        break;
+                    case Key.Space:                        
+                        Torpedos.Add(new Torpedo(Enterprise));
+                        break;
+                }
+            }
         }
     }
 }
--- Spielobjekt.cs
+++ Spielobjekt.cs
@@ -15,8 +15,8 @@ namespace Asteroids
         public double x { get; private set; }
         public double y { get; private set; }
 
-        protected double vx;
-        protected double vy;
+        public double vx;
+        public double vy;
 
         public Spielobjekt(double x, double y, double vx, double vy)
         {
@@ -45,6 +45,7 @@ namespace Asteroids
     class Asteroid : Spielobjekt
     {
         static Random rnd = new Random();
+        Polygon umriss = new Polygon();
 
         public Asteroid(Canvas Zeichenfläche)
             : base( rnd.NextDouble() * Zeichenfläche.ActualWidth,
@@ -52,33 +53,96 @@ namespace Asteroids
                     (rnd.NextDouble() - 0.5) * 200,
                     (rnd.NextDouble() - 0.5) * 200)
         {
-
+            for (int i = 0; i < 15; i++)
+            {
+                double radius = rnd.NextDouble()*8+8;
+                double winkel = 2 * Math.PI / 15 * i;
+                umriss.Points.Add(new Point(radius*Math.Cos(winkel),radius*Math.Sin(winkel)));
+            }
+            umriss.Fill = Brushes.Gray;
         }
 
         public override void Zeichnen(Canvas Zeichenfläche)
         {
-            Polygon umriss = new Polygon();
-            for (int i = 0; i < 7; i++)
-            {
-                double winkel = 2 * Math.PI / 7 * i;
-                umriss.Points.Add(new Point(10*Math.Cos(winkel),10*Math.Sin(winkel)));
-            }
-
-            umriss.Fill = Brushes.Gray;
             Zeichenfläche.Children.Add(umriss);
             Canvas.SetTop(umriss, y);
             Canvas.SetLeft(umriss, x);
         }
     }
 
-    //to do:
-    //class Raumschiff : Spielobjekt
-    //{
+    
+    class Raumschiff : Spielobjekt
+    {
+        Polygon umriss = new Polygon();
 
-    //}
+        public Raumschiff(Canvas Zeichefläche)
+            :base(0.5*Zeichefläche.ActualWidth,
+                  0.5*Zeichefläche.ActualHeight,
+                  0.1,
+                  0.1)
+        {
+            umriss.Points.Add(new Point(0,-10));
+            umriss.Points.Add(new Point(5,7));
+            umriss.Points.Add(new Point(-5,7));
+            umriss.Fill = Brushes.Blue;
+        }
 
-    //class Torpedo : Spielobjekt
-    //{
+        public override void Zeichnen(Canvas Zeichenfläche)
+        {
+            double WinkelInGrad = Math.Atan2(vy,vx) * 180.0 / Math.PI + 90;
+            umriss.RenderTransform = new RotateTransform(WinkelInGrad);
+            Zeichenfläche.Children.Add(umriss);
+            Canvas.SetTop(umriss, y);
+            Canvas.SetLeft(umriss, x);
+        }
 
-    //}
+        public void Beschleunige(bool schneller)
+        {
+            //if(schneller)
+            //{
+            //    vx*=1.1;
+            //    vy*=1.1;
+            //}
+            //else
+            //{
+            //    vx*=0.9;
+            //    vy*=0.9;
+            //}
+            double faktor = schneller ? 1.1 : 0.9;
+            vx*=faktor;
+            vy*=faktor;
+        }
+
+        public void Lenke(bool nachLinks)
+        {
+            double winkel = (nachLinks ? -5 : 5) / 180.0 * Math.PI;
+            double sin = Math.Sin(winkel);
+            double cos = Math.Cos(winkel);
+            double vxn, vyn;
+            vxn = vx * cos - vy*sin;
+            vyn = vx * sin + vy*cos;
+            vx=vxn;
+            vy=vyn;
+        }
+    }
+    
+    class Torpedo : Spielobjekt
+    {
+        public Torpedo(Raumschiff schiff)
+            :base(schiff.x, schiff.y, 2*schiff.vx, 2*schiff.vy)
+        {
+
+        }
+
+        public override void Zeichnen(Canvas Zeichenfläche)
+        {
+            Ellipse e = new Ellipse();
+            e.Width = 5;
+            e.Height = 5;
+            e.Fill = Brushes.Red;
+            Zeichenfläche.Children.Add(e);
+            Canvas.SetTop(e,y-2.5);
+            Canvas.SetLeft(e,x-2.5);
+        }
+    }
 }
